c36a274fa7bbd870bad0ca8455e7c213e0dbdcc8
Temesgen abebayehu <130055841+temesgensoftware@users.noreply.github.com>
2023-04-07 16:55:22 +0300

    Update Pythagorean triple.cpp

--- Pythagorean triple.cpp
+++ Pythagorean triple.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main() {
   cout<<"side1"<<'\t'<<"side2"<<'\t'<<"hypotnenuse"<<endl;
-      for(int i=1;i<=100;i++){
-        for(int j=1;j<=100;j++){
-          for(int k=1;k<=100;k++){
-            if(pow(i,2)+pow(j,2)==pow(k,2)){
+      for(int i=1;i<=100;i++) {
+        for(int j=1;j<=100;j++) {
+          for(int k=1;k<=100;k++) {
+            if(pow(i,2)+pow(j,2)==pow(k,2)) {
               cout<<" "<<i<<'\t'<<" "<<j<<'\t'<<" "<<k<<endl;
             }
           }
